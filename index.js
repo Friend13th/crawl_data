bd4a17600da95cc357e93136b7de8816c8645fc4
Hugie <pmdai@cmcglobal.vn>
2022-08-17 17:34:14 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 
 let urlStr = `https://vinabiz.us`;
 
-
 function getListUrlCompany(data) {
   let regexUrl = /href="\/company\/detail(.*?)"/g;
   //   return regexUrl.exec(data);
@@ -27,18 +26,30 @@ function getListUrlCompany(data) {
 
 // Handle data when connect each site
 async function handleData(url) {
-    
+  console.log(`${urlStr}/company/detail${url}`);
+  return axios.get(`${urlStr}/${url}`);
 }
-
+// Return json data from raw data
+function formatData(raw) {
+  let regex = /href="\/company\/detail(.*?)"/g;
+}
+function handleDetail() {}
 (async () => {
   try {
     console.log(`${urlStr}/company`, "url");
     const resp = await axios.get(`${urlStr}/company`);
-    const listUrl = getListUrlCompany(resp.data);
+    let listUrl = getListUrlCompany(resp.data);
     console.log("res ", listUrl);
+    let rawData = await Promise.all(
+      listUrl.map(async (item) => {
+        let res = await handleData(item);
+        return res.data;
+      })
+    );
+    let jsonData = formatData(rawData[0]);
+    console.log(rawData[0]);
   } catch (err) {
     // Handle Error Here
     console.error(err);
   }
 })();
-
